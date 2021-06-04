cd34884e376ebe29179cb5f2ef55442322128224
kevintheriault <77777560+kevintheriault@users.noreply.github.com>
2021-06-04 00:11:15 -0400

    updated output routing to use model

--- src/main/java/sheridan/theriake/assignment1/rockpaperscissors/controller/Routing.java
+++ src/main/java/sheridan/theriake/assignment1/rockpaperscissors/controller/Routing.java
@@ -1,20 +1,26 @@
 package sheridan.theriake.assignment1.rockpaperscissors.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import sheridan.theriake.assignment1.rockpaperscissors.domain.Game;
 
 @Controller
 public class Routing {
     @GetMapping("/Input")
-    public ModelAndView input(){
+    public ModelAndView input() {
 //        return "Input";
         return new ModelAndView("Input", "game", new Game());
     }
 
     @GetMapping("/Output")
-    public String output(){
-        return "Output";
+    public ModelAndView output(@Validated @ModelAttribute Game game, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return new ModelAndView("Input");
+        }
+        return new ModelAndView("Output", "game", game);
     }
 }
