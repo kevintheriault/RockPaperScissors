173c55703c6dde8748aedcb2705f0e9f3e8c7dfe
kevintheriault <77777560+kevintheriault@users.noreply.github.com>
2021-06-03 22:37:53 -0400

    first commit

---
+++ src/test/java/sheridan/theriake/assignment1/rockpaperscissors/RockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.theriake.assignment1.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
