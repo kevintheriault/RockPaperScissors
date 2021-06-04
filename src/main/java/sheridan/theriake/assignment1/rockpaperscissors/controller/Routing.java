70c2e6c9d39aa4f85523f0b0823fcaae741adaa4
kevintheriault <77777560+kevintheriault@users.noreply.github.com>
2021-06-03 23:39:42 -0400

    Imported Game class to the routing

--- src/main/java/sheridan/theriake/assignment1/rockpaperscissors/controller/Routing.java
+++ src/main/java/sheridan/theriake/assignment1/rockpaperscissors/controller/Routing.java
@@ -3,6 +3,7 @@ package sheridan.theriake.assignment1.rockpaperscissors.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import sheridan.theriake.assignment1.rockpaperscissors.domain.Game;
 
 @Controller
 public class Routing {
