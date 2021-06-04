5cbbc9961fa906a90e3d26a64b97c308e7298f1c
kevintheriault <77777560+kevintheriault@users.noreply.github.com>
2021-06-03 23:38:48 -0400

    created game object which will hold player choices.

---
+++ src/main/java/sheridan/theriake/assignment1/rockpaperscissors/domain/Game.java
@@ -0,0 +1,36 @@
+package sheridan.theriake.assignment1.rockpaperscissors.domain;
+
+import java.io.Serializable;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+public class Game implements Serializable {
+
+    @Min(0)
+    @Max(2)
+    @NotBlank
+    int choice;
+
+    public Game() {
+    }
+
+    public Game(String choice){
+        this.choice = choice;
+    }
+
+    public int getChoice() {
+        return choice;
+    }
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "choice=" + choice +
+                '}';
+    }
+}
